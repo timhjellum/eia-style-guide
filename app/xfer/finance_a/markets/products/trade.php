1383f86762886d9841c8dc160b0b4b93712e2829
timhjellum <timhjellum@gmail.com>
2025-11-17 08:51:08 -0800

    sync

---
+++ app/xfer/finance_a/markets/products/trade.php
@@ -0,0 +1,37 @@
+<?php
+$locale 		= 'petprod';
+$linkPage 		= 'trade';
+$globalTitle 	= 'U.S. Energy Information Administration (EIA)';
+$pageTitle 		= 'What drives petroleum product prices: Trade';
+$l2id 			= 10;
+?>
+<html>
+
+<head>
+	<title><?=$pageTitle?> - <?=$globalTitle?></title>
+	<meta property="og:title" content="<?=$pageTitle?> - <?=$globalTitle?>">
+	<meta property="og:url" content="https://www.eia.gov<?=$_SERVER['SCRIPT_NAME']?>">
+	<meta name="url" content="https://www.eia.gov<?=$_SERVER['SCRIPT_NAME']?>">
+	<?php
+	$doNotIncludeHighcharts = true;
+	include('global/head/includes/head.inc');
+	include('../includes/header_efmi.inc');
+	include('../includes/highcharts.inc');
+	?>
+	<link rel="stylesheet" href="includes/efmi.css" type="text/css">
+	<link rel="stylesheet" href="includes/efmi.css" type="text/css">
+</head>
+
+<body>
+	<?php
+include('global/header/includes/header.inc');
+include('./includes/report-header.inc');
+include('./includes/sub-navigation.inc');
+include('./includes/dashboard.inc');
+include('./includes/tertiary-navigation.inc');
+include('./includes/trade.inc');
+include('global/footer/includes/footer.inc');
+?>
+</body>
+
+</html>
